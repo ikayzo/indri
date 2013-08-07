6b9439acd95bdf421cd44d04a2b198a777c4c31e
Nathan Dwyer <nathan@ikayzo.com>
2013-08-07 11:02:01 -1000

    Added command line arguments

--- nodefilesystem.js
+++ nodefilesystem.js
@@ -1,5 +1,5 @@
 
-var rootDir = '.';
+var rootDir = '/tmp';
 
 var fs = require('fs');
 var path = require('path');
@@ -22,46 +22,53 @@ function handleFileRequest(req, res) {
 
 	var result = {};
 
-	if(action == "parent") {
-		if(loc != "/") {
-			loc = path.dirname(loc);
-		}
-		result.loc = loc;
-	}
-	else if(action == "browse") {
-		result.realLoc = constrainPath(path.join(rootDir, loc));
-		result.loc = loc = result.realLoc.slice(rootDir.length);
-
-		result.contents = [];
-		fs.readdirSync(result.realLoc).forEach(function(fileName) {
-			if(isValidFile(fileName)) {
-				var stats = fs.statSync(path.join(result.realLoc, fileName));
-				var fileInfo = { 
-					name: fileName,
-					location: path.join(loc, fileName),
-					isDir: stats.isDirectory(),
-					size: (stats.isFile() ? stats.size : 0),
-					created: stats.ctime.getTime(),
-					modified: stats.mtime.getTime(),
-				};
-				result.contents.push(fileInfo);
+	try {
+		if(action == "parent") {
+			if(loc != "/") {
+				loc = path.dirname(loc);
 			}
-		});
+			result.loc = loc;
+		}
+		else if(action == "browse") {
+			result.realLoc = constrainPath(path.join(rootDir, loc));
+			result.loc = loc = result.realLoc.slice(rootDir.length);
+
+			result.contents = [];
+			fs.readdirSync(result.realLoc).forEach(function(fileName) {
+				if(isValidFile(fileName)) {
+					var stats = fs.statSync(path.join(result.realLoc, fileName));
+					var fileInfo = { 
+						name: fileName,
+						location: path.join(loc, fileName),
+						isDir: stats.isDirectory(),
+						size: (stats.isFile() ? stats.size : 0),
+						created: stats.ctime.getTime(),
+						modified: stats.mtime.getTime(),
+					};
+					result.contents.push(fileInfo);
+				}
+			});
+		}
+		else {
+			result.error = "Invalid action";
+			result.action = action;
+			result.loc = loc;
+		}
 	}
-	else {
-		result.error = "Invalid action";
-		result.action = action;
-		result.loc = loc;
+	catch(ex) {
+		result.error = "Error accessing " + loc;
+		result.exception = ex;
 	}
 
-	var stats = fs.statSync(rootDir);
 	res.end(JSON.stringify(result) + '\n');
-
 }
+
+var serverName = (process.argv.length > 2) ? process.argv[2] : "localhost";
+var serverPort = (process.argv.length > 3) ? process.argv[3] : 1337;
 
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
   handleFileRequest(req, res);
-}).listen(1337, '192.168.0.31');
-console.log('Server running at http://192.168.0.31:1337/');
+}).listen(serverPort, serverName);
+console.log('Server running at http://' + serverName + ':' + serverPort);
