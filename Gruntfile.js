b5ce4bb6171dea5ef21954473f0059b49d30cf82
LupineDev <chris@lupinedev.com>
2013-09-19 09:27:05 -1000

    Minifiy css in grunt and added version numbers in filenames

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,13 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        banner: '/*! <%= pkg.name %>-<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
         mangle: false,
         preserveComments: false
       },
       build: {
         files: {
-          'build/<%= pkg.name %>.min.js': [
+          'build/<%= pkg.name %>-<%= pkg.version %>.min.js': [
             'js/indri.fsm.js',
             'js/indri.base.js',
             'js/indri.content.renderer.base.js',
@@ -26,11 +26,22 @@ module.exports = function(grunt) {
           'css/indri.css': 'css/indri.scss'
         }
       }
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/* <%= pkg.name %>-<%= pkg.version %> minified css file */'
+        },
+        files: {
+          'build/<%= pkg.name %>-<%= pkg.version %>.min.css': ['css/indri.css']
+        }
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['sass', 'uglify']);
+  grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
 };
