1a86a85107443a69ada756b2a138c2731a4a4eeb
LupineDev <chris@lupinedev.com>
2013-09-20 16:37:49 -1000

    Moved over nathan's foundation example

---
+++ javascripts/test-dlg.js
@@ -0,0 +1,50 @@
+/*
+	Dialog test page helper javascript
+*/
+
+function getDialogInitializer(defaults) {
+	var initializer = jQuery.extend(true, {}, defaults);
+	initStandardExtensions(initializer);
+
+	var fsLookup = {
+		local : initLocalFileSystem,
+		ikayzo : initAwsFileSystem,
+		civilbeat : initCivilBeatFileSystem,
+	};
+
+	var fs = jQuery('#fileSystem').find('option:selected').val();
+	fsLookup[fs](initializer);
+
+	console.log(initializer);
+	return initializer;
+
+	function initStandardExtensions(initializer) {
+		initializer.filter.options.push({ value: "\.(htm|html)$", text: "HTML files (*.htm, *.html)"});
+		initializer.filter.options.push({ value: "\.js$", text: "Javascript files (*.js)"});
+		initializer.filter.options.push({ value: "\.(jpg|png)", text: "Image files (*.jpeg, *.jpg, *.png)"});
+
+		initializer.viewFactory.views.push(new IconContentRenderer());
+		initializer.viewFactory.views.push(new DetailContentRenderer());
+	}
+
+	function initLocalFileSystem(initializer) {
+		initializer.locationRenderer = new SegmentedLocationRenderer();
+		initializer.visibility.shortcutsPanel = true;
+	}
+
+	function initAwsFileSystem(initializer) {
+		initializer.visibility.shortcutsPanel = true;
+
+		initializer.locationRenderer = new BucketLocationRenderer();
+
+		initializer.previewRenderer.baseUrl = 'http://ikayzo-files.s3.amazonaws.com/';
+	}
+
+	function initCivilBeatFileSystem(initializer) {
+		initializer.visibility.shortcutsPanel = true;
+
+		initializer.locationRenderer = new BucketLocationRenderer();
+
+		initializer.previewRenderer.baseUrl = 'http://civilbeat_production.s3.amazonaws.com/';
+	}
+}
