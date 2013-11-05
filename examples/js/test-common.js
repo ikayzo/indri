043845cfc81cf23e1be45ce25c1aac133fb995c9
Nathan Dwyer <nathan@ikayzo.com>
2013-11-05 10:27:22 -1000

    Cleaned up example files

--- examples/js/test-common.js
+++ examples/js/test-common.js
@@ -4,47 +4,20 @@
 
 function getDialogInitializer(defaults) {
 	var initializer = jQuery.extend(true, {}, defaults);
-	initStandardExtensions(initializer);
 
-	var fsLookup = {
-		local : initLocalFileSystem,
-		ikayzo : initAwsFileSystem,
-		civilbeat : initCivilBeatFileSystem,
-	};
+	// Standard settings
+	initializer.filter.options.push({ value: "\.(htm|html)$", text: "HTML files (*.htm, *.html)"});
+	initializer.filter.options.push({ value: "\.js$", text: "Javascript files (*.js)"});
+	initializer.filter.options.push({ value: "\.(jpg|png)", text: "Image files (*.jpg, *.png)"});
 
-	var fs = jQuery('#fileSystem').find('option:selected').val();
-	fsLookup[fs](initializer);
+	initializer.viewFactory.views.push(new IconContentRenderer());
+	initializer.viewFactory.views.push(new DetailContentRenderer());
+
+	// Settings for talking to the heroku app
+	initializer.locationRenderer = new SegmentedLocationRenderer();
+	initializer.visibility.shortcutsPanel = true;
+	initializer.previewRenderer.baseUrl = 'http://indri-filesystem.herokuapp.com/';
 
 	console.log(initializer);
 	return initializer;
-
-	function initStandardExtensions(initializer) {
-		initializer.filter.options.push({ value: "\.(htm|html)$", text: "HTML files (*.htm, *.html)"});
-		initializer.filter.options.push({ value: "\.js$", text: "Javascript files (*.js)"});
-		initializer.filter.options.push({ value: "\.(jpg|png)", text: "Image files (*.jpg, *.png)"});
-
-		initializer.viewFactory.views.push(new IconContentRenderer());
-		initializer.viewFactory.views.push(new DetailContentRenderer());
-	}
-
-	function initLocalFileSystem(initializer) {
-		initializer.locationRenderer = new SegmentedLocationRenderer();
-		initializer.visibility.shortcutsPanel = true;
-		initializer.previewRenderer.baseUrl = 'http://indri-filesystem.herokuapp.com/';
-	}
-
-	function initAwsFileSystem(initializer) {
-		initializer.visibility.shortcutsPanel = true;
-
-		initializer.locationRenderer = new BucketLocationRenderer();
-
-	}
-
-	function initCivilBeatFileSystem(initializer) {
-		initializer.visibility.shortcutsPanel = true;
-
-		initializer.locationRenderer = new BucketLocationRenderer();
-
-		initializer.previewRenderer.baseUrl = 'http://civilbeat_production.s3.amazonaws.com/';
-	}
 }
